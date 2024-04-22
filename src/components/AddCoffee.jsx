80f0e2ccfc302ded55b845cf4235e8ba78e3d531
sujonifty <sujonifty@gmail.com>
2024-04-22 08:22:18 +0600

    first commit

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h1>add a coffee</h1>
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h1>update coffee</h1>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
